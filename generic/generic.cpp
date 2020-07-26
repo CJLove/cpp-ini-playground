ba3304a1fc632f59530ff003c3a8057ab8285a63
Chris Love <christopherlove68@gmail.com>
2020-07-26 15:12:13 -0700

    Updates

--- generic/generic.cpp
+++ generic/generic.cpp
@@ -34,25 +34,33 @@ int main(int argc, char**argv)
  
     inipp::Ini<char> ini;
     std::ifstream is(file);
-    try {
-        ini.parse(is);
+    ini.parse(is);
+
+    if (ini.errors.size()) {
+        for (const auto &s: ini.errors) {
+            std::cout << "Invalid input: " << s << "\n";
+        }
+    } else {
 
         std::string topString;
         inipp::extract(ini.sections["DEFAULT"]["top-string"],topString);
         std::cout << "top-string = " << topString << "\n";
 
         int topInt = 0;
-        inipp::extract(ini.sections["DEFAULT"]["top-int"],topInt);
-        std::cout << "top-int = " << topInt << "\n";
+        bool result = inipp::extract(ini.sections["DEFAULT"]["top-int"],topInt);
+        std::cout << "top-int = " << topInt << " result = " << result << "\n";
+
+        result = inipp::extract(ini.sections["foo"]["blah"],topInt);
+        std::cout << "result for bad section/name = " << result << "\n";
 
         bool topBool = false;
         inipp::extract(ini.sections["DEFAULT"]["top-bool"],topBool);
         std::cout << "top-bool = " << topBool << "\n";
 
-    }
-    catch (std::exception &e)
-    {
-        std::cout << "Caught exception " << e.what() << "\n";
+        std::string name;
+        inipp::extract(ini.sections["test"]["name"],name);
+        std::cout << "name = " << name << "\n";
+
     }
 
 }
